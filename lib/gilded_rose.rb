b450f9090433765af84e4172efed0ead2f6e06cd
Valentin Sanjuan <valesanjuan9@gmail.com>
2020-03-29 17:00:10 -0300

    My solution to the Gilded Rose Kata

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,47 +8,112 @@ class GildedRose
   end
 
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
+    item = get_item_class.new(@quality, @days_remaining)
+    item.tick
+    @days_remaining = item.days_remaining
+    @quality = item.quality
+  end
+
+  def get_item_class
+    if  @name == 'Normal Item'
+      NormalItem
     else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
+      GildedRose.const_get(
+        @name
+          .gsub(' ', '_')
+          .gsub(',', '_')
+          .split('_')
+          .collect(&:capitalize)
+          .join
+          .+('Item')
+      )
+    end
+  end
+
+  class NormalItem
+    attr_reader :quality, :days_remaining
+
+    def initialize quality, days_remaining
+      @quality = quality
+      @days_remaining = days_remaining
+    end
+
+    def tick
+      decrease_quality
+      @days_remaining -= 1
+      if @days_remaining < 0
+        decrease_quality
+      end
+    end
+
+    private
+
+    def decrease_quality
+      @quality = @quality - 1 if @quality > 0
+    end
+  end
+
+  class SulfurasHandOfRagnarosItem < NormalItem
+    def tick
+    end
+  end
+
+  class BackstagePassesToATafkal80etcConcertItem < NormalItem
+    def tick
+      increase_quality
+      @days_remaining -= 1
+      if @days_remaining < 0
+        decrease_quality
+      end
+    end
+
+    private
+
+    def decrease_quality
+      destroy if @days_remaining < 0
+    end
+
+    def increase_quality
+      if @quality < 50 
+        @quality += 1
+        if @days_remaining < 11
+          @quality += 1
+        end
+        if @days_remaining < 6
+          @quality += 1
         end
       end
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
+
+    def destroy
+      @quality = 0
     end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
+  end
+
+  class AgedBrieItem < NormalItem
+    def tick
+      increase_quality
+      @days_remaining -= 1
+      if @days_remaining < 0
+        increase_quality
+      end
+    end
+
+    private
+
+    def increase_quality
+      @quality += 1 if @quality < 50
+    end
+  end
+
+  class ConjuredManaCakeItem < NormalItem
+    def tick
+      decrease_quality
+      decrease_quality
+      @days_remaining -= 1
+      if @days_remaining < 0
+        decrease_quality
+        decrease_quality
       end
     end
   end
